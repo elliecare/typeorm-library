b855c18ad9dbdfb96b4cc3d37b92e95e3e92527a
Danilo Prieto <danilo.s.prieto@gmail.com>
2025-05-16 11:09:26 -0300

    [FIX] create static token entity

--- index.ts
+++ index.ts
@@ -49,8 +49,9 @@ import {Sensors_audit} from './src/entity/sensors_audit.entity';
 import { ConnectModule } from './src/entity/modules/connect.module.entity';
 import { WhatsappModule } from './src/entity/modules/whatsapp.module.entity';
 import { CompanyClientModuleAccess } from './src/entity/modules/module_access.entity';
+import { StaticToken } from './src/entity/static_token.entity';
 
-export const Entities = [CompanyClientModuleAccess, WhatsappModule, ConnectModule, Sensors_audit, WifiConnected, PhoneLine, PasswordResetToken, Steps,Alert_audit,CompanyClient,InviteCode,Config_audit,Health_system_audit,Config,Gateway_audit,EventEntity,DiscountCode, Gateway,HealthSystem,HeartRate,LineProvider,Battery,Location,Measurement,Message,Notification,OffBody,OperationalRole,PlansDescription,Purchase,Role_SupportNetwork,User_audit,User,Smartwatch_audit,Smartwatch,Role_support_network_audit, Sensors,Tracking,Support_network_audit,SupportNetwork, Tracking_audit, Users_smartwatches_audit, UserSmartwatch, Plans, PatientAgentContact, UserOperationalRole, Plans_audit, AccessCode, Alert, AlertConfiguration, AlertLogs]
+export const Entities = [StaticToken, CompanyClientModuleAccess, WhatsappModule, ConnectModule, Sensors_audit, WifiConnected, PhoneLine, PasswordResetToken, Steps,Alert_audit,CompanyClient,InviteCode,Config_audit,Health_system_audit,Config,Gateway_audit,EventEntity,DiscountCode, Gateway,HealthSystem,HeartRate,LineProvider,Battery,Location,Measurement,Message,Notification,OffBody,OperationalRole,PlansDescription,Purchase,Role_SupportNetwork,User_audit,User,Smartwatch_audit,Smartwatch,Role_support_network_audit, Sensors,Tracking,Support_network_audit,SupportNetwork, Tracking_audit, Users_smartwatches_audit, UserSmartwatch, Plans, PatientAgentContact, UserOperationalRole, Plans_audit, AccessCode, Alert, AlertConfiguration, AlertLogs]
 
 export { Plans };
 export { PatientAgentContact };
@@ -103,3 +104,4 @@ export { Sensors_audit };
 export { ConnectModule };
 export { WhatsappModule };
 export { CompanyClientModuleAccess };
+export { StaticToken };
