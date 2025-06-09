5e7664692ebcd51795c2973789e27412aaf2b286
Danilo Prieto <danilo.s.prieto@gmail.com>
2025-06-09 14:58:55 -0300

    [FEAT] Add MailingModule

---
+++ src/entity/modules/mailing.module.entity.ts
@@ -0,0 +1,41 @@
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	UpdateDateColumn,
+	DeleteDateColumn,
+	ManyToOne,
+	JoinColumn,
+	CreateDateColumn,
+} from "typeorm";
+import { CompanyClient } from "../company_client.entity";
+
+@Entity()
+export class MailingModule {
+	@PrimaryGeneratedColumn("uuid")
+	id: string;
+
+	@ManyToOne(() => CompanyClient, client => client.id)
+	@JoinColumn({ name: "company_client_id" })
+	company_client: CompanyClient;
+
+	@Column({
+		type: Boolean,
+		default: false,
+	})
+	enabled: boolean;
+
+	@Column({
+		nullable: true,
+	})
+	email: string;
+
+	@CreateDateColumn()
+	created_at: Date;
+
+	@UpdateDateColumn()
+	updated_at: Date;
+
+	@DeleteDateColumn()
+	deleted_at: Date;
+}
