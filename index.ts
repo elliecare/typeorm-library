da9d472c3275bddf54e298c70763eedac4b9aeef
Danilo Prieto <danilo.s.prieto@gmail.com>
2025-06-09 10:18:56 -0300

    [FEAT] add commercial_module

--- index.ts
+++ index.ts
@@ -52,8 +52,9 @@ import { Company_client_audit } from './src/entity/company_client_audit.entity';
 import { StaticToken } from './src/entity/static_token.entity';
 import { ConnectModule_audit } from './src/entity/modules/connect.module_audit.entity';
 import { WhatsappModule_audit } from './src/entity/modules/whatsapp.module_audit.entity';
+import { CommercialModule } from 'src/entity/modules/commercial.module.entity';
 
-export const Entities = [StaticToken, ConnectModule_audit, WhatsappModule_audit, Company_client_audit, WhatsappModule, ConnectModule, Sensors_audit, WifiConnected, PhoneLine, PasswordResetToken, Steps,Alert_audit,CompanyClient,InviteCode,Config_audit,Health_system_audit,Config,Gateway_audit,EventEntity,DiscountCode, Gateway,HealthSystem,HeartRate,LineProvider,Battery,Location,Measurement,Message,Notification,OffBody,OperationalRole,PlansDescription,Purchase,Role_SupportNetwork,User_audit,User,Smartwatch_audit,Smartwatch,Role_support_network_audit, Sensors,Tracking,Support_network_audit,SupportNetwork, Tracking_audit, Users_smartwatches_audit, UserSmartwatch, Plans, PatientAgentContact, UserOperationalRole, Plans_audit, AccessCode, Alert, AlertConfiguration, AlertLogs]
+export const Entities = [CommercialModule ,StaticToken, ConnectModule_audit, WhatsappModule_audit, Company_client_audit, WhatsappModule, ConnectModule, Sensors_audit, WifiConnected, PhoneLine, PasswordResetToken, Steps,Alert_audit,CompanyClient,InviteCode,Config_audit,Health_system_audit,Config,Gateway_audit,EventEntity,DiscountCode, Gateway,HealthSystem,HeartRate,LineProvider,Battery,Location,Measurement,Message,Notification,OffBody,OperationalRole,PlansDescription,Purchase,Role_SupportNetwork,User_audit,User,Smartwatch_audit,Smartwatch,Role_support_network_audit, Sensors,Tracking,Support_network_audit,SupportNetwork, Tracking_audit, Users_smartwatches_audit, UserSmartwatch, Plans, PatientAgentContact, UserOperationalRole, Plans_audit, AccessCode, Alert, AlertConfiguration, AlertLogs]
 
 export { Plans };
 export { PatientAgentContact };
@@ -109,3 +110,4 @@ export { Company_client_audit };
 export { ConnectModule_audit };
 export { WhatsappModule_audit };
 export { StaticToken };
+export { CommercialModule };
