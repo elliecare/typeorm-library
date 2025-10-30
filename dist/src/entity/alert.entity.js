27e0df1bc23b12c5a0a672750f23085279391d45
Danilo Prieto <danilo.s.prieto@gmail.com>
2025-10-30 13:16:17 -0300

    [FEAT] alert.entity add porp connect_status

--- dist/src/entity/alert.entity.js
+++ dist/src/entity/alert.entity.js
@@ -160,6 +160,12 @@ __decorate([
     __metadata("design:type", Boolean)
 ], Alert.prototype, "true_event", void 0);
 __decorate([
+    (0, typeorm_1.Column)({
+        nullable: true,
+    }),
+    __metadata("design:type", String)
+], Alert.prototype, "connect_status", void 0);
+__decorate([
     (0, typeorm_1.CreateDateColumn)(),
     __metadata("design:type", Date)
 ], Alert.prototype, "created_at", void 0);
